cc680736af10c32847563bab5410d6253e485971
sebastian-porling <sebastian.porling@gmail.com>
2018-04-28 09:34:18 +0200

    First commit

---
+++ src/util/Amount.java
@@ -0,0 +1,16 @@
+package util;
+
+/**
+ * Describes the amount of something. In this case an integer.
+ */
+public class Amount {
+    int amount;
+
+    /**
+     * Creates an object that describes the amount of something.
+     * @param amount amount of something as an integer.
+     */
+    public Amount(int amount){
+        this.amount = amount;
+    }
+}
