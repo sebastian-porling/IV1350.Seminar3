4c24bed682c5c5ef4d2c900ba3e3923e01c45091
sebastian-porling <sebastian.porling@gmail.com>
2018-05-02 14:39:55 +0200

    The 'Main' class is now calling all the right methods/classes.

--- src/startup/Main.java
+++ src/startup/Main.java
@@ -1,7 +1,28 @@
 package startup;
 
-public class Main {
-    public static void main(String[] args) {
+import controller.Controller;
+import integration.CatalogCreator;
+import integration.Printer;
+import integration.SystemCreator;
+import view.View;
 
+/**
+ * Contains the <code>main</code> method. Performs all startup of the
+ * application.
+ */
+public class Main {
+
+    /**
+     * Starts the application.
+     *
+     * @param args The application does not take any command line parameters.
+     */
+    public static void main(String[] args) {
+        SystemCreator systemCreator = new SystemCreator();
+        CatalogCreator catalogCreator = new CatalogCreator();
+        Printer printer = new Printer();
+        Controller controller = new Controller(systemCreator, catalogCreator, printer);
+        View view = new View(controller);
+        view.sampleExecution();
     }
 }
