3144c408b49086def2687e5bedf27e4cb7262241
sebastian-porling <sebastian.porling@gmail.com>
2018-05-02 14:38:01 +0200

    Removed duplicate classes, they had the same function as the class 'Amount', added comments to 'Amount'

--- src/integration/ItemDTO.java
+++ src/integration/ItemDTO.java
@@ -1,4 +1,7 @@
 package integration;
 
+/**
+ * 
+ */
 public class ItemDescription {
 }
--- src/util/Amount.java
+++ src/util/Amount.java
@@ -1,19 +1,25 @@
 package util;
 
 /**
- * Represent the amount of money.
+ * Represent the amount of something, like money or items.
  */
 public class Amount {
     private int amount;
 
     /**
      * Creates an instance, representing the specified amount.
+     *
      * @param amount The amount represented by the newly created instance.
      */
     public Amount(int amount){
         this.amount = amount;
     }
 
+    /**
+     * Get the value of amount.
+     *
+     * @return The value of amount.
+     */
     public int getAmount() {
         return amount;
     }
