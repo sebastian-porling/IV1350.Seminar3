8b37f7709e63fbdf0447c8c5152fbe032d1eaaf5
sebastian-porling <sebastian.porling@gmail.com>
2018-05-03 14:37:26 +0200

    Done with all code that doesn't have any dependecies to other layers.

--- src/integration/AccountingSystem.java
+++ src/integration/AccountingSystem.java
@@ -1,4 +1,22 @@
 package integration;
 
+import model.Sale;
+
+/**
+ * Represents an external dummy accounting system
+ */
 public class AccountingSystem {
+
+    /**
+     *  Creates a new instance, representing a dummy accounting system.
+     */
+    AccountingSystem(){ }
+
+    /**
+     *
+     * @param saleInfo
+     */
+    public void bookKeep(Sale sale){
+
+    }
 }
--- src/integration/CatalogCreator.java
+++ src/integration/CatalogCreator.java
@@ -1,4 +1,34 @@
 package integration;
 
+/**
+ * Represents a creator that consists of different catalogs.
+ */
 public class CatalogCreator {
+    private DiscountCatalog discountCatalog;
+    private ItemCatalog itemCatalog;
+
+    /**
+     * Creates a new instance
+     */
+    public CatalogCreator(){
+        discountCatalog = new DiscountCatalog();
+        itemCatalog = new ItemCatalog();
+    }
+
+    /**
+     * Gets the value of discountCatalog.
+     *
+     * @return The value discountCatalog.
+     */
+    public DiscountCatalog getDiscountCatalog() {
+        return discountCatalog;
+    }
+
+    /**
+     * Gets the value of itemCatalog.
+     * @return  The value of itemCatalog.
+     */
+    public ItemCatalog getItemCatalog() {
+        return itemCatalog;
+    }
 }
