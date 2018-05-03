28e33b284fba4e9a6715d605530b3944b9a59a04
sebastian-porling <sebastian.porling@gmail.com>
2018-05-03 14:39:51 +0200

    Layer almost done, have to add a sample execution

--- src/view/View.java
+++ src/view/View.java
@@ -1,4 +1,25 @@
 package view;
 
+import controller.Controller;
+
+/**
+ *
+ */
 public class View {
+    private Controller controller;
+
+    /**
+     *
+     * @param controller
+     */
+    public View(Controller controller){
+        this.controller = controller;
+    }
+
+    /**
+     *
+     */
+    public void sampleExecution(){
+
+    }
 }
