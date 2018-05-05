d2d70c6a7e6905df7004fbe980c89424b52fe794
sebastian-porling <sebastian.porling@gmail.com>
2018-05-05 13:17:34 +0200

    Controller ready for testing, have all necessery javadoc

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -2,12 +2,13 @@ package controller;
 
 import integration.*;
 import model.CashRegister;
+import model.Payment;
+import model.Receipt;
 import model.Sale;
 import util.Amount;
-import util.CustomerIdentification;
 
 /**
- *  The pos applications only controller, all calls to the model is passed through here.
+ *  The Point-of-sale applications only controller, all calls to the model is passed through here.
  */
 public class Controller {
     private AccountingSystem accountingSystem;
@@ -19,7 +20,7 @@ public class Controller {
     private Sale sale;
 
     /**
-     * Creates a new instance.
+     * Creates a new instance, represented as a controller.
      *
      * @param systemCreator     Used to get all classes that handles external system calls.
      * @param catalogCreator    Used to get all classes that handles database calls.
@@ -34,24 +35,57 @@ public class Controller {
         this.cashRegister = new CashRegister();
     }
 
+    /**
+     *  Initiates a new sale.
+     */
     public void startNewSale(){
         this.sale = new Sale();
     }
 
+    /**
+     *  If the item identifier exists we will add it to the sale and return information about the item and show the running total.
+     *  Else we will just show the running total
+     *
+     * @param itemIdentifier The item we are going to add to the sale.
+     * @param quantity The amount of that item.
+     * @return If <code>Item</code> exists we return a string with information about the item and the running total, else we return a string with the running total.
+     */
     public String registerItem(String itemIdentifier, Amount quantity){
-        return displaySale();
+        if (itemCatalog.itemExists(itemIdentifier)){
+            Item item = itemCatalog.getItem(itemIdentifier, quantity);
+            return sale.updateSale(item) + ", quantity: " + quantity.toString() +  ", running total:  " + displayTotal();
+        }
+        return "running total: " + displayTotal();
     }
 
-    public String displaySale(){
-        return "";
+    /**
+     *  Displays the total with taxes calculated into it.
+     *
+     * @return The total with taxes as a String.
+     */
+    public String displayTotalWithTax(){
+        return "total with taxes: " + sale.getTotal().getTotalWithTax().toString();
     }
 
-    public String lookForDiscount(CustomerIdentification customerIdentification){
-        return displaySale();
+    private String displayTotal(){
+        return sale.getTotal().getTotal().toString();
     }
 
-    public void pay(Amount paidAmount){
-
+    /**
+     * Makes a payment with the given <code>Amount</code>. Will be added to the balance of the cashRegister.
+     * The external system will be updated, and a receipt will be created and printed by the printer.
+     *
+     * @param paidAmount The amount of money given by the customer.
+     * @return The total change that will be given to the customer.
+     */
+    public String pay(Amount paidAmount){
+        Payment payment = new Payment(paidAmount, sale.getTotal());
+        cashRegister.addPayment(payment);
+        accountingSystem.bookKeep(sale);
+        inventorySystem.updateInventory(sale);
+        Receipt receipt = new Receipt(sale);
+        printer.printReceipt(receipt);
+        return "Change: " + payment.getChange().toString();
     }
 
 }
